3db9cd5e56dfb451732e55a31451e825afd08a68
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-18 16:03:26 +0900

    Title: 데이터 교환, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250118/데이터 교환/exchange-data.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        System.out.println("7");
+        System.out.println("5");
+        System.out.println("6");
+    }
+}
