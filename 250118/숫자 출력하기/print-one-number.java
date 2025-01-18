90d3c8be439e3afaff61da9068e5a454115b5eee
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-18 15:15:04 +0900

    Title: 숫자 출력하기, Time: 81ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250118/숫자 출력하기/print-one-number.java
@@ -0,0 +1,11 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         // Please write your code here.
+//     }
+// }
+
+public class Main{
+    public static void main(String args[]){
+        System.out.print(3);
+    }
+}
