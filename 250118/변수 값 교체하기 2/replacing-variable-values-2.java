a64ea42c4d8ebfc88ec8985a27895e49a3f2065c
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-18 15:56:47 +0900

    Title: 변수 값 교체하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250118/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a =5;
+        int b=3;
+
+        a=b;
+        system.out.println("3");
+
+        system.out.println("3");
+    }
+}
