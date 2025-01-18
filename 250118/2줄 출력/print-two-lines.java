90cdc8190510b1cc467a25a379ff4ddfb07200ff
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-18 15:13:57 +0900

    Title: 2줄 출력, Time: 86ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250118/2줄 출력/print-two-lines.java
@@ -0,0 +1,13 @@
+// public class Main {
+//     public static void main(String[] args) {
+//         // Please write your code here.
+//     }
+// }
+
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("World");
+    }
+}
