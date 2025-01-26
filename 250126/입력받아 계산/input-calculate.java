1c241c2c763bd19ae33af8e2681c96ac0712ea73
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-26 20:21:13 +0900

    Title: 입력받아 계산, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250126/입력받아 계산/input-calculate.java
@@ -0,0 +1,21 @@
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         // Please write your code here.
+//         Scanner sc =  new Scanner(System.in);
+//         int a;
+//         a = sc.nextInt();
+//         System.out.println(a+2);
+//     }
+// }
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        System.out.println(a+2);
+    }
+}
