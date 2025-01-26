93752931d4de1dacf96b65091afa8ac421e0c45f
hangyel123 <116795778+hangyel123@users.noreply.github.com>
2025-01-26 20:36:21 +0900

    Title: 입력받아 출력 2, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 250126/입력받아 출력 2/input-and-output-2.java
+++ 250126/입력받아 출력 2/input-and-output-2.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new Scanner();
+        Scanner sc = new Scanner(System.in);
         String s = sc.next();
         String[] strArr = s.split("-");
 
